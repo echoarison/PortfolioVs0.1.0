c7904a2cd0e809c944989e4eb2f1952ae618a344
Patric Alison <alisson_gamer2000@outlook.com>
2023-05-17 15:08:24 -0300

    update pages and backEnd.js

--- Js/backEnd.js
+++ Js/backEnd.js
@@ -3,3 +3,20 @@ function EmBreve(){
 	alert("Em breve será adicionado!");
 
 }
+
+function AlertAv(value){
+
+	//variavel local
+	resp = parseInt(value);
+
+	switch(value)
+	{
+		case 1:
+			alert("Infelizmente, o blog esta fora do Ar!!!");
+		break;
+
+		default:
+			alert("Pagina não encontrada!");
+		break;		
+	}
+}
